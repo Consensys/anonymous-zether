12ea593f14544f893f0319f05a7ce1ba88c121f6
Benjamin Diamond <30356252+benediamond@users.noreply.github.com>
2019-03-14 14:29:08 -0400

    big changes in alt_bn128 library
    first of all, changed all wrappers for precompiles to inline assembly. this will save huge gas costs. also, fixed mapInto functions: they all work.

--- zether/contracts/alt_bn128.sol
+++ zether/contracts/alt_bn128.sol
@@ -3,6 +3,7 @@ pragma experimental ABIEncoderV2;
 
 library alt_bn128 {
 
+    uint256 public constant P = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
     uint256 public constant q = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
 
     struct G1Point {
@@ -10,36 +11,33 @@ library alt_bn128 {
         uint256 Y;
     }
 
-    function add(G1Point memory p1, G1Point memory p2) internal view returns (G1Point memory r) {
-        uint256[4] memory input;
-        input[0] = p1.X;
-        input[1] = p1.Y;
-        input[2] = p2.X;
-        input[3] = p2.Y;
+    function add(G1Point memory p1, G1Point memory p2) public view returns (G1Point memory r) {
         assembly {
-            if iszero(staticcall(not(0), 6, input, 0x80, r, 0x40)) {
+            let m := mload(0x40)
+            mstore(m, mload(p1))
+            mstore(add(m, 0x20), mload(add(p1, 0x20)))
+            mstore(add(m, 0x40), mload(p2))
+            mstore(add(m, 0x60), mload(add(p2, 0x40)))
+            if iszero(staticcall(gas, 0x06, m, 0x80, r, 0x40)) {
                 revert(0, 0)
             }
         }
     }
 
     function mul(G1Point memory p, uint256 s) internal view returns (G1Point memory r) {
-        uint256[3] memory input;
-        input[0] = p.X;
-        input[1] = p.Y;
-        input[2] = s;
         assembly {
-            if iszero(staticcall(not(0), 7, input, 0x60, r, 0x40)) {
+            let m := mload(0x40)
+            mstore(m, mload(p))
+            mstore(add(m, 0x20), mload(add(p, 0x20)))
+            mstore(add(m, 0x40), s)
+            if iszero(staticcall(gas, 0x07, m, 0x60, r, 0x40)) {
                 revert(0, 0)
             }
         }
     }
 
     function neg(G1Point memory p) internal view returns (G1Point memory) {
-        uint n = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
-        if (p.X == 0 && p.Y == 0)
-            return G1Point(0, 0);
-        return G1Point(p.X, n - (p.Y % n));
+        return G1Point(p.X, P - (p.Y % P)); // p.Y should already be reduced mod P?
     }
 
     function eq(G1Point memory p1, G1Point memory p2) internal pure returns (bool) {
@@ -70,40 +68,59 @@ library alt_bn128 {
         return q - x;
     }
 
-    function exp(uint256 base, uint256 exponent) internal view returns (uint256 output) {
-        uint256[6] memory input;
-        input[0] = 0x20;  // length_of_BASE
-        input[1] = 0x20;  // length_of_EXPONENT
-        input[2] = 0x20;  // length_of_MODULUS
-        input[3] = base;
-        input[4] = exponent;
-        input[5] = q;
+    function exp(uint256 base, uint256 exponent) internal view returns (uint256 output) { // warning: mod p, not q
+        uint256 q = q;
         assembly {
-            if iszero(staticcall(not(0), 5, input, 0xc0, output, 0x20)) {
+            let m := mload(0x40)
+            mstore(m, 0x20)
+            mstore(add(m, 0x20), 0x20)
+            mstore(add(m, 0x40), 0x20)
+            mstore(add(m, 0x60), base)
+            mstore(add(m, 0x80), exponent)
+            mstore(add(m, 0xa0), q)
+            if iszero(staticcall(gas, 0x05, m, 0xc0, m, 0x20)) { // staticcall or call?
                 revert(0, 0)
             }
-        } // careful: i modified this a bit.
+            output := mload(m)
+        }
+    }
+
+    function fieldexp(uint256 base, uint256 exponent) internal view returns (uint256 output) { // warning: mod p, not q
+        uint256 P = P;
+        assembly {
+            let m := mload(0x40)
+            mstore(m, 0x20)
+            mstore(add(m, 0x20), 0x20)
+            mstore(add(m, 0x40), 0x20)
+            mstore(add(m, 0x60), base)
+            mstore(add(m, 0x80), exponent)
+            mstore(add(m, 0xa0), P)
+            if iszero(staticcall(gas, 0x05, m, 0xc0, m, 0x20)) { // staticcall or call?
+                revert(0, 0)
+            }
+            output := mload(m)
+        }
     }
     
     function mapInto(uint256 seed) internal view returns (G1Point memory) { // warning: function totally untested!
         uint256 y;
         while (true) {
-            uint256 ySquared = add(exp(seed, 3), 3); // should already handle mods
-            y = exp(ySquared, (q + 1) / 4);
-            if (exp(y, 2) == ySquared) {
+            uint256 ySquared = fieldexp(seed, 3) + 3; // addmod instead of add: waste of gas, plus function overhead cost
+            y = fieldexp(ySquared, (P + 1) / 4);
+            if (fieldexp(y, 2) == ySquared) {
                 break;
             }
             seed += 1;
         }
-        return G1Point(seed, y); // no checking for valid point
+        return G1Point(seed, y);
     }
 
     function mapInto(string calldata input) external view returns (G1Point memory) { // warning: function totally untested!
-        return mapInto(mod(uint256(keccak256(abi.encodePacked(input)))));
+        return mapInto(uint256(keccak256(abi.encodePacked(input))) % P);
     }
 
     function mapInto(string calldata input, uint256 i) external view returns (G1Point memory) { // warning: function totally untested!
-        return mapInto(mod(uint256(keccak256(abi.encodePacked(input, i)))));
+        return mapInto(uint256(keccak256(abi.encodePacked(input, i))) % P);
         // ^^^ important: i haven't tested this, i.e. whether it agrees with ProofUtils.paddedHash(input, i) (cf. also the go version)
     }
 }
