57c428dbcc34e3b8a3d27e078039c914d7fca85d
Benjamin Diamond <30356252+benediamond@users.noreply.github.com>
2019-03-16 13:02:11 -0400

    variable names

--- zether/contracts/ZetherVerifier.sol
+++ zether/contracts/ZetherVerifier.sol
@@ -18,8 +18,8 @@ contract ZetherVerifier {
     uint256[m] twos = powers(2);
 
     struct ZetherStatement {
-        alt_bn128.G1Point[] balanceCommitNewL;
-        alt_bn128.G1Point[] balanceCommitNewR;
+        alt_bn128.G1Point[] CL;
+        alt_bn128.G1Point[] CR;
         alt_bn128.G1Point[] L;
         alt_bn128.G1Point R;
         alt_bn128.G1Point[] y;
@@ -82,13 +82,13 @@ contract ZetherVerifier {
     function verify(bytes32[2][] memory CL, bytes32[2][] memory CR, bytes32[2][] memory L, bytes32[2] memory R, bytes32[2][] memory y, uint256 epoch, bytes32[2] memory u, bytes memory proof) view public returns (bool) {
         ZetherStatement memory statement;
         uint256 size = y.length;
-        statement.balanceCommitNewL = new alt_bn128.G1Point[](size);
-        statement.balanceCommitNewR = new alt_bn128.G1Point[](size);
+        statement.CL = new alt_bn128.G1Point[](size);
+        statement.CR = new alt_bn128.G1Point[](size);
         statement.L = new alt_bn128.G1Point[](size);
         statement.y = new alt_bn128.G1Point[](size);
         for (uint256 i = 0; i < size; i++) {
-            statement.balanceCommitNewL[i] = alt_bn128.G1Point(uint256(CL[i][0]), uint256(CL[i][1]));
-            statement.balanceCommitNewR[i] = alt_bn128.G1Point(uint256(CR[i][0]), uint256(CR[i][1]));
+            statement.CL[i] = alt_bn128.G1Point(uint256(CL[i][0]), uint256(CL[i][1]));
+            statement.CR[i] = alt_bn128.G1Point(uint256(CR[i][0]), uint256(CR[i][1]));
             statement.L[i] = alt_bn128.G1Point(uint256(L[i][0]), uint256(L[i][1]));
             statement.y[i] = alt_bn128.G1Point(uint256(y[i][0]), uint256(y[i][1]));
         }
@@ -153,7 +153,7 @@ contract ZetherVerifier {
         if (proof.size % 2 != 0) { return false; } // "Anonymity set size must be even!"
 
         ZetherAuxiliaries memory zetherAuxiliaries;
-        zetherAuxiliaries.y = uint256(keccak256(abi.encode(keccak256(abi.encode(statement.epoch, statement.R, statement.balanceCommitNewL, statement.balanceCommitNewR, statement.L, statement.y)), proof.A, proof.S))).mod();
+        zetherAuxiliaries.y = uint256(keccak256(abi.encode(keccak256(abi.encode(statement.epoch, statement.R, statement.CL, statement.CR, statement.L, statement.y)), proof.A, proof.S))).mod();
         // warning: not correct as written. the encoding will include length headers, whereas the java version does not!
         zetherAuxiliaries.ys = powers(zetherAuxiliaries.y);
         zetherAuxiliaries.z = uint256(keccak256(abi.encode(zetherAuxiliaries.y))).mod();
@@ -211,8 +211,8 @@ contract ZetherVerifier {
             anonAuxiliaries.L2[i][1] = anonAuxiliaries.L2[i][1].add(anonProof.LG[i][1]).mul(anonAuxiliaries.xInv);
         }
         for (uint256 i = 0; i < proof.size; i++) {
-            anonAuxiliaries.balanceCommitNewL2 = anonAuxiliaries.balanceCommitNewL2.add(statement.balanceCommitNewL[i].mul(anonAuxiliaries.f[i][0]));
-            anonAuxiliaries.balanceCommitNewR2 = anonAuxiliaries.balanceCommitNewR2.add(statement.balanceCommitNewR[i].mul(anonAuxiliaries.f[i][0]));
+            anonAuxiliaries.balanceCommitNewL2 = anonAuxiliaries.balanceCommitNewL2.add(statement.CL[i].mul(anonAuxiliaries.f[i][0]));
+            anonAuxiliaries.balanceCommitNewR2 = anonAuxiliaries.balanceCommitNewR2.add(statement.CR[i].mul(anonAuxiliaries.f[i][0]));
             anonAuxiliaries.parity = anonAuxiliaries.parity.add(statement.y[i].mul(anonAuxiliaries.cycler[i][0].mul(anonAuxiliaries.cycler[i][1])));
         }
 
