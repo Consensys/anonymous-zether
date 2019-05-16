10290823f680b3fa4f86fdf972dd43296172758a
Benjamin Diamond <30356252+benediamond@users.noreply.github.com>
2019-05-16 12:43:41 -0400

    add prover folder

---
+++ prover/src/main/java/zkp/AppConfig.java
@@ -0,0 +1,25 @@
+package zkp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+
+}
---
+++ prover/src/main/java/zkp/utilities/Util.java
@@ -0,0 +1,44 @@
+package zkp.utilities;
+
+public class Util {
+
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    public static byte[] hexStringToByteArray(String s) {
+        s = s.substring(2).toUpperCase();
+        int len = s.length();
+        if (len % 2 == 1){
+            len++;
+            s = "0" + s;
+        }
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
+
+
+    public static byte[][] hexStringsToByteArrays(String s) { // assumes that s is 0x + a concatenation of 64-byte hex strings!
+        s = s.substring(2).toUpperCase();
+        int size = s.length() / 128;
+        byte[][] data = new byte[size][128];
+        for (int i = 0; i < size; i++) {
+            data[i] = hexStringToByteArray("0x" + s.substring(i * 128, (i + 1) * 128));
+        }
+        return data;
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2 + 2];
+        hexChars[0] = '0';
+        hexChars[1] = 'x';
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2 + 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 3] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+}
