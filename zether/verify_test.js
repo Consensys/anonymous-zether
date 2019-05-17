e411247cb945d95dba5970a411c400976c52ec65
Benjamin Diamond <30356252+benediamond@users.noreply.github.com>
2019-05-17 10:42:07 -0400

    beginning the refactoring process

--- zether/verify_test.js
+++
@@ -1,6 +0,0 @@
-// CLn CRn is copyed from Java console
-
-CLn = ["0x0d146a5d0596655ddd4105c6d3231459bee3d6d6765fa00aca3b07a8a7d05d2c", "0x0edf1c4cd912ff18f993de582d57bbc0c505d0d52683df9653dbb734636248be"]
-CRn = ["0x16d22f2a8ba41492a80b3438914c228eba1c7b8bb2ca466430a1cafaddbba8a1", "0x2a0be7cda8d990d5fb59d168f4de42752662f5f4daa6eb3792476826604bb732"]
-
-zkp.verifyTransfer(CLn, CRn, result['outL'], result['inL'], result['inOutR'], y, yBar, result['proof'])
