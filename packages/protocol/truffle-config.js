0590de900efc7037c50774510393c23c3c9f7a95
Nicolae Leonte <nicolae.leonte.go@gmail.com>
2022-08-18 12:12:16 +0100

    update dependency versions and add quorum examples truffle test configuration

--- packages/protocol/truffle-config.js
+++ packages/protocol/truffle-config.js
@@ -6,6 +6,13 @@ module.exports = {
             gasPrice: 0,
             network_id: "*", // Match any network id
             websockets: true,
+        },
+        qex: {
+            host: "127.0.0.1",
+            port: 22000, // node1 in quorum examples
+            gasPrice: 0,
+            network_id: "*",
+            websockets: true,
         }
     },
     compilers: {
